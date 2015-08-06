c1d44c2e4fea9f8dc2fb8eabfd42b0d833880955
Ramesh Viswanathan <v-ramesh@users.noreply.github.com>
2015-08-06 19:10:33 -0400

    Completed all four R scripts and generation of four plots.

---
+++ plot1.R
@@ -0,0 +1,25 @@
+#LOAD DATA
+
+# Unzipped data file saved as "data/household_power_consumption.txt"
+filename <- file.path("data", "household_power_consumption.txt")
+
+# Subsetting the data for the two days of interest directly to avoid memory
+# issues with storing the whole dataset in memory
+data_two_days <- subset(read.csv(filename, sep=";", na.strings="?", 
+                                 stringsAsFactors=FALSE),
+                        Date %in% c("1/2/2007", "2/2/2007"))
+
+# Create a new column datetime in POSIXlt class format from 
+# Date and Time columns
+data_two_days$datetime = strptime(paste(data_two_days$Date, 
+                                        data_two_days$Time, sep = " "),
+                                  format = "%d/%m/%Y %T")
+
+# PLOT DATA
+
+png(filename = "plot1.png", width = 480, height = 480, bg = "transparent")
+hist(data_two_days$Global_active_power, col = "red", 
+     main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
+
+
